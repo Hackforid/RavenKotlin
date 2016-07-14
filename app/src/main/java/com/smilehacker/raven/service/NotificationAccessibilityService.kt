1f9a58b66b3110491393c6754ee7543512d94113
kleist <zhouquanbest@gmail.com>
2016-07-15 00:27:01 +0800

    add notification accessibility service

--- app/src/main/java/com/smilehacker/raven/service/NotificationAccessibilityService.kt
+++ app/src/main/java/com/smilehacker/raven/service/NotificationAccessibilityService.kt
@@ -1,18 +1,37 @@
 package com.smilehacker.raven.service
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.app.Notification
 import android.view.accessibility.AccessibilityEvent
+import com.smilehacker.raven.voice.TTSManager
 
 /**
  * Created by kleist on 16/6/30.
  */
 class NotificationAccessibilityService : AccessibilityService() {
+
     override fun onInterrupt() {
-        throw UnsupportedOperationException()
     }
 
-    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
-        throw UnsupportedOperationException()
+    override fun onServiceConnected() {
+        val info = AccessibilityServiceInfo()
+        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_AUDIBLE
+        info.notificationTimeout = 100
+        serviceInfo = info
+    }
+
+    override fun onAccessibilityEvent(e: AccessibilityEvent?) {
+        if (e == null) {
+            return
+        }
+        if (e.eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
+            val data = e.parcelableData
+            if (data is Notification) {
+                TTSManager.readText(e.packageName.toString(), data.tickerText.toString())
+            }
+        }
     }
 
 }
