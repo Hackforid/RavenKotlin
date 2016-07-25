dc1fb1151ae069394fb32fec54c16d4c39883d3b
kleist <zhouquanbest@gmail.com>
2016-07-26 01:10:55 +0800

    add preference

--- app/src/main/java/com/smilehacker/raven/ui/index/preference/ConfigActivity.kt
+++ app/src/main/java/com/smilehacker/raven/ui/index/preference/ConfigActivity.kt
@@ -1,10 +1,17 @@
 package com.smilehacker.raven.ui.index.preference
 
+import android.os.Bundle
 import android.preference.PreferenceActivity
+import com.smilehacker.raven.R
 
 /**
  * Created by zhouquan on 16/7/25.
  */
 class ConfigActivity: PreferenceActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.preference)
+    }
 
 }
